f466e6d989393d79fc1589c8d9b3554f80269b0b
guzel-kabirova <vkcom1998@gmail.com>
2021-10-14 22:49:27 +0300

    Implemented increasing percent and decreasing liters click by click. Second click empties the small cup of water

--- 16_DrinkWater/script.js
+++ 16_DrinkWater/script.js
@@ -1,4 +1,28 @@
 const cups = document.querySelectorAll('.small-cup');
+const remained = document.getElementById('remained');
+const liters = document.getElementById('liters');
+const percent = document.querySelector('.percent');
+
+const updateLiters = () => {
+    const fullCups = document.querySelectorAll('.small-cup.full').length;
+    const allCups = cups.length;
+    if (fullCups === 0) {
+        percent.style.visibility = 'hidden';
+        percent.style.height = '0';
+    } else {
+        percent.style.visibility = 'visible';
+        percent.style.height = `${400 * (fullCups / allCups)}px`;
+        percent.innerText = `${100 * (fullCups / allCups)}%`;
+    }
+    if (fullCups === allCups) {
+        remained.style.visibility = 'hidden';
+        remained.style.height = '0';
+    } else {
+        remained.style.visibility = 'visible';
+        remained.style.height = `${400 * (1 - fullCups / allCups)}px`;
+        liters.innerText = `${2 - (250 * fullCups / 1000)}л`;
+    }
+};
 
 const highlightCups = (idxClick) => {
     if (cups[idxClick].classList.contains('full')
@@ -10,8 +34,14 @@ const highlightCups = (idxClick) => {
             cup.classList.add('full');
         } else cup.classList.remove('full');
     });
+    updateLiters();
 };
 
+updateLiters();
+
 cups.forEach((cup, idxClick) => {
-    cup.addEventListener('click', () => highlightCups(idxClick));
+    cup.addEventListener('click', () => {
+        highlightCups(idxClick);
+    });
 });
+
